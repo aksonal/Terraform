cc150b65b88cce2c59ac43ff5d41d381a341905b
aksonal <41297988+aksonal@users.noreply.github.com>
2025-06-17 01:55:25 +0530

    Create main.tf

---
+++ multiple-resources-wthout-module/SG/SG-4/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+    region = "us-east-1"
+    #profile = "vau-dev-test"
+}
+
+variable "sg_rules" {
+  description = "Security group definitions"
+  type = map(object({
+    ingress = map(list(string))  # port => list of CIDRs
+    egress  = map(list(string))  # port => list of CIDRs
+  }))
+  default = {
+    sg_1 = {
+      ingress = {
+        80  = ["10.0.0.0/16"]
+        443 = ["10.0.1.0/24"]
+      }
+      egress = {
+        88 = ["0.0.0.0/0"]
+        53 = ["8.8.8.8/32"]
+      }
+    },
+    sg_2 = {
+      ingress = {
+        5432 = ["172.16.0.0/12"]
+        22   = ["192.168.1.0/24"]
+      }
+      egress = {
+        443 = ["0.0.0.0/0"]
+        123 = ["1.1.1.1/32"]
+      }
+    }
+  }
+}
+
+
+#security group
+resource "aws_security_group" "allow_tls" {
+  for_each = var.sg_rules
+  name        = each.key
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound" {
+  security_group_id = aws_security_group.allow_tls[each.value.sg_name].id
+
+  for_each = { 
+              for item in local.ingress_rule:
+              item.key => item 
+             } 
+
+  cidr_ipv4   = each.value.cidr
+  from_port   = each.value.port
+  to_port     = each.value.port
+  ip_protocol = "tcp"
+}
+
+
+#egress rule
+resource "aws_vpc_security_group_egress_rule" "allow_outbound" {
+  security_group_id = aws_security_group.allow_tls[each.value.sg_name].id
+
+  for_each = { 
+              for item in local.egress_rule:
+              item.key => item 
+             } 
+
+  cidr_ipv4   = each.value.cidr
+  from_port   = each.value.port
+  ip_protocol = "tcp"
+  to_port     = each.value.port
+}
+
