6a8537814afd6792eb9c1fd598e84d3f32427766
aksonal <41297988+aksonal@users.noreply.github.com>
2026-01-02 16:52:02 +0530

    Update eventbridge-rule.tf
    Added target

--- for_each/Lambda-Functions/eventbridge-rule.tf
+++ for_each/Lambda-Functions/eventbridge-rule.tf
@@ -6,3 +6,10 @@ resource "aws_cloudwatch_event_rule" "eventbrodge_rules" {
     description = each.value.rule_desc
     schedule_expression = each.value.rule_trigger_time
 }
+
+#eventbridge targer
+resource "aws_cloudwatch_event_target" "eventbridge_rule_targets" {
+    for_each = var.eventbridge_targets
+    arn  = aws_lambda_function.lambda_functions[each.value.lambda_key].arn
+    rule = aws_cloudwatch_event_rule.eventbrodge_rules[each.value.rule_key].name
+}
